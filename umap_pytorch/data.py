259b39753f64249928ea56577bf48739082a513f
EC2 Default User <ec2-user@ip-172-31-28-45.ec2.internal>
2025-08-22 18:43:11 +0000

    decrease memory usage

--- umap_pytorch/data.py
+++ umap_pytorch/data.py
@@ -31,23 +31,34 @@ def get_graph_elements(graph_, n_epochs):
 class UMAPDataset(Dataset):
     def __init__(self, data, graph_, n_epochs=200):
         graph, epochs_per_sample, head, tail, weight, n_vertices = get_graph_elements(graph_, n_epochs)
-        
+
+        self.head = head
+        self.tail = tail
+        self.epochs_per_sample = epochs_per_sample
+        self.total = epochs_per_sample.sum()
+        '''
         self.edges_to_exp, self.edges_from_exp = (
-        np.repeat(head, epochs_per_sample.astype("int")),
-        np.repeat(tail, epochs_per_sample.astype("int")),
-    )
+            np.repeat(head, epochs_per_sample.astype("int")),
+            np.repeat(tail, epochs_per_sample.astype("int")),
+        )
         shuffle_mask = np.random.permutation(np.arange(len(self.edges_to_exp)))
         self.edges_to_exp = self.edges_to_exp[shuffle_mask].astype(np.int64)
         self.edges_from_exp = self.edges_from_exp[shuffle_mask].astype(np.int64)
+        '''
         self.data = torch.Tensor(data)
         
     def __len__(self):
-        return int(self.data.shape[0])
+        return int(self.total)
+        #return int(self.data.shape[0])
     
     def __getitem__(self, index):
-        edges_to_exp = self.data[self.edges_to_exp[index]]
-        edges_from_exp = self.data[self.edges_from_exp[index]]
-        return (edges_to_exp, edges_from_exp)
+        # Instead of expanding, sample one edge
+        edge_idx = np.random.choice(len(self.head), p=self.epochs_per_sample / self.total)
+        return self.data[self.head[edge_idx]], self.data[self.tail[edge_idx]]
+    
+        #edges_to_exp = self.data[self.edges_to_exp[index]]
+        #edges_from_exp = self.data[self.edges_from_exp[index]]
+        #return (edges_to_exp, edges_from_exp)
     
 class MatchDataset(Dataset):
     def __init__(self, data, embeddings):
